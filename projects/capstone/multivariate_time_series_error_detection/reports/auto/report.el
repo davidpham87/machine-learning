a5f3d551d8f568924137df5b9ec8ba07b0a2adae
David Pham <davidpham87@gmail.com>
2018-09-15 11:08:50 +0200

    Update

--- projects/capstone/multivariate_time_series_error_detection/reports/auto/report.el
+++ projects/capstone/multivariate_time_series_error_detection/reports/auto/report.el
@@ -5,12 +5,12 @@
                      '(("article" "a4paper" "twoside")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "margin=3.5cm") ("hyperref" "colorlinks")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -33,27 +33,27 @@
     "geometry"
     "pdflscape")
    (LaTeX-add-labels
-    "sec:orgc8da660"
-    "sec:org01d0b7f"
-    "sec:orgab986c4"
-    "sec:org81b92f9"
-    "sec:org822fc2e"
+    "sec:org49f5cc4"
+    "sec:orgbb3d104"
+    "sec:org4beb1d4"
+    "sec:org2b16581"
+    "sec:org271634d"
     "fig:distribution-sectors"
     "fig:gics-level"
-    "sec:orgd0f2fb2"
-    "sec:orgee61770"
-    "sec:orgdb3cd2d"
-    "sec:org3a39f6d"
-    "sec:org0b799ed"
-    "sec:orgeb514e6"
-    "sec:org89a789b"
+    "sec:org68d454b"
+    "sec:org789fa4d"
+    "sec:org028bbfd"
+    "sec:orga6824d6"
+    "sec:org529cc81"
+    "sec:org4d037f0"
+    "sec:orgf5864a6"
     "fig:keras-nn"
-    "sec:orgbd084ae"
+    "sec:org7898ae1"
     "fig:confusion-matrix"
-    "tab:org5b707ed"
-    "sec:orgc85a26b"
+    "tab:orgef0f799"
+    "sec:orgfb77745"
     "fig:tsne-embedding"
     "fig:silhouette-score"
-    "sec:org2ffb474"))
+    "sec:org888f928"))
  :latex)
 
